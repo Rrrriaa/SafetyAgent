26028ce5df324565f7f82765d3ec9789b2bc8114
김희진 <67904074+Rrrriaa@users.noreply.github.com>
2021-09-30 00:22:28 +0900

    torch

--- Assets/KHJ/Scripts/Torch.cs
+++ Assets/KHJ/Scripts/Torch.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Torch : MonoBehaviour
 {
+    Animator ani;
     void Start()
     {
-        
+        ani = GetComponent<Animator>();
     }
 
     void Update()
@@ -14,7 +15,14 @@ public class Torch : MonoBehaviour
         //베터리가 껴져있고, 손에 들고있다면
         if (weldingSceneMngr.instance.isBattery && GetComponent<KHJ_Item>().isGrab)
         {
-
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch))
+            { 
+                ani.SetTrigger("On");            
+            }
+            if (OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch))
+            {
+                ani.SetTrigger("Off");
+            }
         }
     }
 }
